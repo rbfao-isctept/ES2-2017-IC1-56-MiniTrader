74d6f4f55ce68c6bb1d493a3cc32e95bbe0e5c43
Rodrigo Ferreira <rbfao@iscte.pt>
2017-03-15 15:35:33 +0000

    Commit para o video final

--- src/main/java/mt/client/impl/MicroTraderClientImpl.java
+++ src/main/java/mt/client/impl/MicroTraderClientImpl.java
@@ -11,7 +11,7 @@ import mt.comm.impl.ClientCommImpl;
 
 public class MicroTraderClientImpl implements MicroTraderClient {
 	
-	//Commit para video final
+	//Commit para video
 	
 	public static void main(String args[]) {
 		ClientComm clientComm = new ClientCommImpl();
