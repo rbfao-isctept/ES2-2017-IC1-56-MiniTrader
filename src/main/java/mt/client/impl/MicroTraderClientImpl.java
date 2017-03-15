98c820f945607b0fde103542b61be85a4df5ef8e
Rodrigo Ferreira <rbfao@iscte.pt>
2017-03-15 15:42:52 +0000

    First Commit

--- src/main/java/mt/client/impl/MicroTraderClientImpl.java
+++ src/main/java/mt/client/impl/MicroTraderClientImpl.java
@@ -12,7 +12,7 @@ import mt.comm.impl.ClientCommImpl;
 public class MicroTraderClientImpl implements MicroTraderClient {
 	
 	
-	//Commit
+	//Fisrt Commit 
 	
 	public static void main(String args[]) {
 		ClientComm clientComm = new ClientCommImpl();
