9ff6dce89a7c8c8e3c3fed9ec1b9342c98eb57b4
João Rocha <jmofr@iscte-iul.pt>
2017-05-09 11:30:39 +0100

    Restriction cant buy or sell less than 10 units

--- src/main/java/mt/server/MicroServer.java
+++ src/main/java/mt/server/MicroServer.java
@@ -233,6 +233,10 @@ public class MicroServer implements MicroTraderServer {
 		if (o.isSellOrder()) {
 			processSell(msg.getOrder());
 		}
+		// restriction cant buy or sell less than 10 units 
+		if(o.getNumberOfUnits() < 10){
+			throw new ServerException("You can't buy/sell less than 10 units!");
+		}
 
 		// notify clients of changed order
 		notifyClientsOfChangedOrders();
