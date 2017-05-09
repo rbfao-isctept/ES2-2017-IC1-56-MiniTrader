503015e3f21af9e1108862b4e38d4d8e13252c34
João Rocha <jmofr@iscte-iul.pt>
2017-05-09 11:25:31 +0100

    Branch Creation

--- src/main/java/mt/server/MicroServer.java
+++ src/main/java/mt/server/MicroServer.java
@@ -27,6 +27,8 @@ import mt.filter.AnalyticsFilter;
  * @author Group 78
  *
  */
+// criado o Branch 
+
 public class MicroServer implements MicroTraderServer {
 	
 	public static void main(String[] args) {
